ae3c79ec427090a81a0c78a13b6853b705d50907
Joaquin Blanco <100656622+joackob@users.noreply.github.com>
2022-07-28 18:55:18 -0300

    add react-native-vector-icons

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { Text, View } from "react-native";
 import tw from "twrnc";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default function App() {
   return (
     <View style={tw`bg-violet-800 flex-col grow justify-center items-center `}>
       <Text style={tw`font-sans text-6xl font-bold text-white text-center`}>
-        Welcome to Home!!!!
+        Welcome to Home!
       </Text>
+      <Icon name="home" style={tw`text-white text-6xl font-bold`} />
     </View>
   );
 }
